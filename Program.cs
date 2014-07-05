17b7d3438bad00625f52cd792ce528607c78d09d
Jacob Schlitz <jschlitz@gmail.com>
2014-07-05 19:31:42 -0400

    classiving n vs. all

--- Program.cs
+++ Program.cs
@@ -17,127 +17,150 @@ namespace LFD_LibSvm
     const int THRESH = 100000000;
     static readonly double sr10 = Math.Sqrt(10);
 
-    static svm_node[] DatumToNodes(Datum d)
-    {
-      return d.x.Select((lf, i) => new svm_node
-                  {
-                    index = i + 1, //I think that index may need to be 1-based?
-                    value = lf
-                  }).ToArray();
-    }
 
     static void Main(string[] args)
     {
       //read files
       var pwd = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-      var train = ReadProblem(Path.Combine(pwd, "train.txt"));
-      var test = ReadProblem(Path.Combine(pwd, "test.txt"));
-      
+      var train = ReadFile(Path.Combine(pwd, "train.txt"));
+      var test = ReadFile(Path.Combine(pwd, "test.txt"));
 
 
-      var wTarget = (new[] { 0.6, 0.8});
-//      var C = 10.0;
-//      var gamma = 10.0;
-      var w0Target = 0.21; //I don't know how to make this have a bias. What.
-      Datum[] classified = { };
-      for (int experiment = 0; experiment < EXPERIMENTS; experiment++)
+      var results = Enumerable.Range(0, 10)
+        .Select(n => VsAll(n + " vs. all", train, test, KernelHelper.PolynomialKernel(2, 1, 1), 0.01, lf => lf == (double)n ? 1.0 : -1.0))
+        .ToArray();//force.
+
+      foreach (var item in results)
       {
-        try
-        {
-          classified = Generate(TRIALS, w0Target, wTarget);
+        Console.WriteLine(item);
+      }
 
-          var testData = Generate(TRIALS * 10, w0Target, wTarget);
-          var problem = new svm_problem
-          {
-            l = classified.Length,
-            x = classified.Select(DatumToNodes).ToArray(), 
-            y = classified.Select(d => d.y).ToArray(),
-          };
+      Console.ReadKey(true);
 
 
-          double goodC = 0.0;
-          double goodGamma = 0.0;
-          double goodAcc = 0.0;
+      //var wTarget = (new[] { 0.6, 0.8});
+      //var w0Target = 0.21; //I don't know how to make this have a bias. What.
+      //Datum[] classified = { };
+      //for (int experiment = 0; experiment < EXPERIMENTS; experiment++)
+      //{
+      //  try
+      //  {
+      //    classified = Generate(TRIALS, w0Target, wTarget);
 
-          for (int i = 0; i < 100; i++)
-          {
-            classified = Generate(TRIALS, w0Target, wTarget);
-            problem = new svm_problem
-            {
-              l = classified.Length,
-              x = classified.Select(DatumToNodes).ToArray(),
-              y = classified.Select(d => d.y).ToArray(),
-            };
-            var machine0 = new C_SVC(problem, KernelHelper.LinearKernel(), double.MaxValue);
-          }
-
-
-          for (double C = 1/64.0; C <= 32768; C *= 2)
-          {
-            //for (double gamma = .00001; gamma <= 101; gamma *= 10)
-            //{
-            //  Console.WriteLine("------------------------------");
-            //  var machine = new C_SVC(problem, KernelHelper.RadialBasisFunctionKernel(gamma), C);
-            //  var acc = machine.GetCrossValidationAccuracy(5);
-            //  Console.WriteLine("c={0:f3} gamma={1:f3} for {2:f3}", C, gamma, acc);
-            //  if (acc > goodAcc)
-            //  {
-            //    goodC = C;
-            //    goodGamma = gamma;
-            //    goodAcc = acc;
-            //  }
-            //}
-            Console.WriteLine("------------------------------");
-            var machine = new C_SVC(problem, KernelHelper.LinearKernel(), C);
-            //machine.Train();
-            var acc = machine.GetCrossValidationAccuracy(5);
-            var realAcc = ((double)testData.Where(d => machine.Predict(DatumToNodes(d)) == d.y).Count()) / ((double)testData.Length);
-            Console.WriteLine("c={0:f3} acc={1:f3} realAcc={2:f3}", C, acc, realAcc);
-            if (realAcc > goodAcc)
-            {
-              goodC = C;
-              goodAcc = realAcc;
-            }
-          }
+      //    var testData = Generate(TRIALS * 10, w0Target, wTarget);
+      //    var problem = new svm_problem
+      //    {
+      //      l = classified.Length,
+      //      x = classified.Select(DatumToNodes).ToArray(), 
+      //      y = classified.Select(d => d.y).ToArray(),
+      //    };
 
 
-          Console.WriteLine("------------------------------");
-          Console.WriteLine("c={0:f3} gamma={1:f3} for {2:f3}", goodC, goodGamma, goodAcc);
-          //var goodMachine = new C_SVC(problem, KernelHelper.RadialBasisFunctionKernel(goodGamma), goodC);
-          var goodMachine = new C_SVC(problem, KernelHelper.LinearKernel(), goodC);
-          goodMachine.Train();
-          var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "LFD-Svm");
-          Directory.CreateDirectory(path);
-          path = Path.Combine(path, DateTime.Now.ToString("yyyyMMdd-HHmmss-")+experiment);
-          goodMachine.Export(path);
-          Console.ReadKey(true);
-        }
-        catch (Exception ex)
-        {
-          Console.WriteLine(ex.Message);
-        }
+      //    double goodC = 0.0;
+      //    double goodGamma = 0.0;
+      //    double goodAcc = 0.0;
+
+      //    for (int i = 0; i < 100; i++)
+      //    {
+      //      classified = Generate(TRIALS, w0Target, wTarget);
+      //      problem = new svm_problem
+      //      {
+      //        l = classified.Length,
+      //        x = classified.Select(DatumToNodes).ToArray(),
+      //        y = classified.Select(d => d.y).ToArray(),
+      //      };
+      //      var machine0 = new C_SVC(problem, KernelHelper.LinearKernel(), double.MaxValue);
+      //    }
+
+
+      //    for (double C = 1/64.0; C <= 32768; C *= 2)
+      //    {
+      //      Console.WriteLine("------------------------------");
+      //      var machine = new C_SVC(problem, KernelHelper.LinearKernel(), C);
+      //      //machine.Train();
+      //      var acc = machine.GetCrossValidationAccuracy(5);
+      //      var realAcc = ((double)testData.Where(d => machine.Predict(DatumToNodes(d)) == d.y).Count()) / ((double)testData.Length);
+      //      Console.WriteLine("c={0:f3} acc={1:f3} realAcc={2:f3}", C, acc, realAcc);
+      //      if (realAcc > goodAcc)
+      //      {
+      //        goodC = C;
+      //        goodAcc = realAcc;
+      //      }
+      //    }
+
+
+      //    Console.WriteLine("------------------------------");
+      //    Console.WriteLine("c={0:f3} gamma={1:f3} for {2:f3}", goodC, goodGamma, goodAcc);
+      //    //var goodMachine = new C_SVC(problem, KernelHelper.RadialBasisFunctionKernel(goodGamma), goodC);
+      //    var goodMachine = new C_SVC(problem, KernelHelper.LinearKernel(), goodC);
+      //    goodMachine.Train();
+      //    var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "LFD-Svm");
+      //    Directory.CreateDirectory(path);
+      //    path = Path.Combine(path, DateTime.Now.ToString("yyyyMMdd-HHmmss-")+experiment);
+      //    goodMachine.Export(path);
+      //    Console.ReadKey(true);
+      //  }
+      //  catch (Exception ex)
+      //  {
+      //    Console.WriteLine(ex.Message);
+      //  }
+      //}
+    }
+
+    private static TrainInfo VsAll(string info, string[][] train, string[][] test, Kernel kernel, double c, Func<double, double> convertY)
+    {
+      var trainProb = MakeProblem (train,convertY);
+      var machine = new C_SVC(trainProb, kernel, c);
+      var predictions = trainProb.x.Select(x => machine.Predict(x)).ToArray();
+      var wrong1 =predictions.Zip(trainProb.y, (pred, real) => pred * real)
+        .Where(lf => lf < 0.0)
+        .Count();
+      return new TrainInfo { Info = info, Ein = (double)wrong1 / (double)trainProb.l, Machine = machine };
+    }
+
+    private class TrainInfo
+    {
+      public string Info;
+      public double Ein;
+      public double Eout;
+      public override string ToString()
+      {
+        return string.Format("{0} Ein={1:f3} Eout={2:f3}", Info, Ein, Eout);
+      }
+      public C_SVC Machine;
+      public void ExportMachine(string name)
+      {
+        var path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "LFD-Svm");
+        Directory.CreateDirectory(path);
+        path = Path.Combine(path, name + ".xml");
+        Machine.Export(path);
       }
     }
 
-    private static svm_problem ReadProblem(string theFile)
+    private static string[][] ReadFile(string theFile)
     {
       using (var sr = new StreamReader(theFile))
       {
-        var fromFile = sr.ReadToEnd()
+        return sr.ReadToEnd()
           .Split('\n')
-          .Where(s=>!string.IsNullOrWhiteSpace(s))
+          .Where(s => !string.IsNullOrWhiteSpace(s))
           .Select(s => s.Split('\t'))
           .ToArray();
-        return new svm_problem
-        {
-          l = fromFile.Count(),
-          y = fromFile.Select(l => double.Parse(l.First())).ToArray(),
-          x = fromFile.Select(l => l.Skip(1)
-            .Select((a, i) => new svm_node { index = i + 1, value = double.Parse(a) }).ToArray())
-            .ToArray()
-        };
       }
     }
+
+    private static svm_problem MakeProblem(string[][] raw, Func<double,double> convertY)
+    {
+      return new svm_problem
+      {
+        l = raw.Count(),
+        y = raw.Select(l => convertY(double.Parse(l.First()))).ToArray(),
+        x = raw.Select(l => l.Skip(1)
+          .Select((a, i) => new svm_node { index = i + 1, value = double.Parse(a) }).ToArray())
+          .ToArray()
+      };
+    }
+
 
     private static double[] Norm(double[] wPla)
     {
@@ -145,13 +168,6 @@ namespace LFD_LibSvm
       return wPla.Select(x => x / mag).ToArray();
     }
 
-    private static Datum[] Generate(int count, double w0, double[] w)
-    {
-      return Enumerable.Range(1, count)
-        .Select(_ => GetX())
-        .Select(ex => new Datum { x = ex, y = GetY(ex, w0, w) })
-        .ToArray();
-    }
 
     private static double GetY(double[] x, double w0, double[] w)
     {
@@ -174,25 +190,5 @@ namespace LFD_LibSvm
     }
   }
 
-  public struct SVInfo
-  {
-    public double y;
-    public double[] x;
-    public double alpha;
-    public override string ToString()
-    {
-      return string.Join(",", x) + "," + y.ToString() + "," + alpha.ToString();
-    }
-  }
-
-  public struct Datum
-  {
-    public double y;
-    public double[] x;
-    public override string ToString()
-    {
-      return string.Join(",", x) + "," + y.ToString();
-    }
-  }
 
 }
